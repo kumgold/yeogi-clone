850d537189b1cb1f0985f590846288c8ade80a7f
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-06-05 13:37:45 +0900

    Update HomeScreen.kt

--- app/src/main/java/com/test_application/hollyscompose/ui/home/HomeScreen.kt
+++ app/src/main/java/com/test_application/hollyscompose/ui/home/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.test_application.hollyscompose.ui.home
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -10,16 +9,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
