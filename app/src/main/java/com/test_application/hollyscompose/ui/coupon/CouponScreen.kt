46efedcca48509a2f616462275295cc472988a89
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-07-27 22:45:12 +0900

    Update CouponScreen.kt

--- app/src/main/java/com/test_application/hollyscompose/ui/coupon/CouponScreen.kt
+++ app/src/main/java/com/test_application/hollyscompose/ui/coupon/CouponScreen.kt
@@ -1,11 +1,21 @@
 package com.test_application.hollyscompose.ui.coupon
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,13 +40,25 @@ fun CouponScreen(
             style = HollysTypography.h2
         )
 
+        TabRow(selectedTabIndex = 0) {
 
+        }
     }
+}
+
+@Composable
+private fun CouponTabLayout() {
+
 }
 
 @Composable
 @Preview
 private fun CouponScreenPreview() {
+    val list = listOf("사용가능쿠폰", "만료쿠폰")
+    var selectedIndex by remember {
+        mutableStateOf(0)
+    }
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -44,9 +66,30 @@ private fun CouponScreenPreview() {
 
         Text(
             modifier = Modifier.padding(vertical = 20.dp, horizontal = 10.dp),
-            text = "나의 쿠폰",
+            text = stringResource(id = R.string.my_coupon),
             style = HollysTypography.h2
         )
-
+        
+        TabRow(
+            modifier = Modifier.fillMaxWidth(),
+            selectedTabIndex = selectedIndex,
+            indicator = { tabPositions ->
+                Box {}
+            }
+        ) {
+            list.forEachIndexed { index, item ->
+                val selected = (selectedIndex == index)
+                Tab(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(50)),
+                    selected = selected,
+                    onClick = {
+                        selectedIndex = index
+                    }
+                ) {
+                    Text(text = item)
+                }
+            }
+        }
     }
 }
