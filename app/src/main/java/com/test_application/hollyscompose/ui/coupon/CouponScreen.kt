694b65edbe243dcac55b496d99dae366d5eaa844
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-07-30 15:19:11 +0900

    kotlin version upgrade
    kotlin : 1.6.10 -> 1.8.10
    compile sdk 32 -> 33
    target sdk 32 -> 33
    compose option 1.1.1 -> 1.4.2
    
    add compose foundation dependency

--- app/src/main/java/com/test_application/hollyscompose/ui/coupon/CouponScreen.kt
+++ app/src/main/java/com/test_application/hollyscompose/ui/coupon/CouponScreen.kt
@@ -1,21 +1,21 @@
 package com.test_application.hollyscompose.ui.coupon
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -27,12 +27,16 @@ import com.test_application.hollyscompose.R
 import com.test_application.hollyscompose.ui.compose.HollysDefaultTopAppBar
 import com.test_application.hollyscompose.ui.theme.HollysDefaultRoundShape
 import com.test_application.hollyscompose.ui.theme.HollysTypography
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CouponScreen(
     modifier: Modifier,
     navController: NavHostController
 ) {
+    val pagerState = rememberPagerState()
+
     Column(
         modifier = modifier
     ) {
@@ -44,19 +48,25 @@ fun CouponScreen(
             style = HollysTypography.h2
         )
 
-        CouponTabLayout()
+        CouponTabLayout(
+            pagerState = pagerState
+        )
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-private fun CouponTabLayout() {
+private fun CouponTabLayout(
+    pagerState: PagerState
+) {
+    val coroutineScope = rememberCoroutineScope()
     val list = listOf("사용가능쿠폰", "만료쿠폰")
-    var selectedIndex by remember {
-        mutableStateOf(0)
-    }
+    val selectedIndex = pagerState.currentPage
 
+    // Tab row
     TabRow(
-        modifier = Modifier.padding(horizontal = 8.dp)
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
             .clip(HollysDefaultRoundShape)
             .border(
                 width = 1.dp,
@@ -69,27 +79,26 @@ private fun CouponTabLayout() {
         contentColor = MaterialTheme.colors.primary,
         indicator = { }
     ) {
-        list.forEachIndexed { index, item ->
-            val selected = (selectedIndex == index)
+        list.forEachIndexed { index, page ->
+            val selected = (pagerState.currentPage == index)
             Tab(
                 modifier = if (selected) {
                     Modifier
                         .clip(HollysDefaultRoundShape)
                         .background(MaterialTheme.colors.primary)
-                        .padding(vertical = 10.dp)
                 } else {
                     Modifier
                         .clip(HollysDefaultRoundShape)
                         .background(MaterialTheme.colors.background)
-                        .padding(vertical = 10.dp)
                 },
                 selected = selected,
                 onClick = {
-                    selectedIndex = index
+                    coroutineScope.launch { pagerState.animateScrollToPage(index) }
                 }
             ) {
                 Text(
-                    text = item,
+                    modifier = Modifier.padding(vertical = 10.dp),
+                    text = page,
                     color = if (selected) {
                         MaterialTheme.colors.background
                     } else {
@@ -99,14 +108,48 @@ private fun CouponTabLayout() {
             }
         }
     }
+
+    // Page
+    HorizontalPager(
+        modifier = Modifier.background(MaterialTheme.colors.background),
+        pageCount = list.size,
+        state = pagerState,
+        verticalAlignment = Alignment.Top
+    ) { index ->
+        when (list[index]) {
+            "사용가능쿠폰" -> {
+                Text(
+                    text = "사용가능쿠폰"
+                )
+            }
+            "만료쿠폰" -> {
+                Text(
+                    text = "만료쿠폰"
+                )
+            }
+        }
+    }
 }
 
+@Composable
+private fun AvailableCouponListScreen() {
+
+}
+
+@Composable
+private fun ExpiredCouponListScreen() {
+
+}
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 @Preview
 private fun CouponScreenPreview() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
+        val pagerState = rememberPagerState()
+
         HollysDefaultTopAppBar {  }
 
         Text(
@@ -115,6 +158,6 @@ private fun CouponScreenPreview() {
             style = HollysTypography.h2
         )
 
-        CouponTabLayout()
+        CouponTabLayout(pagerState = pagerState)
     }
 }
