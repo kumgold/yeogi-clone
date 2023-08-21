4a6a5887f7176c91f2f28be1776e09b601b96057
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-08-21 23:10:02 +0900

    add compose + live data state dependency

--- app/src/main/java/com/test_application/hollyscompose/ui/coupon/coupon_detail/CouponDetail.kt
+++ app/src/main/java/com/test_application/hollyscompose/ui/coupon/coupon_detail/CouponDetail.kt
@@ -1,13 +1,17 @@
 package com.test_application.hollyscompose.ui.coupon.coupon_detail
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.test_application.hollyscompose.data.Coupon
 
 @Composable
 fun CouponDetail(
     couponId: Long
 ) {
     val viewModel = viewModel<CouponDetailViewModel>()
+    val coupon = viewModel.coupon.observeAsState(viewModel.getCoupon(couponId))
 
-    viewModel.getCoupon(couponId)
+    
 }
