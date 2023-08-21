02707d842282c9f96e44704ce9df15ad06e0b0b1
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-08-21 23:10:29 +0900

    Update CouponDetail.kt

--- app/src/main/java/com/test_application/hollyscompose/ui/coupon/coupon_detail/CouponDetail.kt
+++ app/src/main/java/com/test_application/hollyscompose/ui/coupon/coupon_detail/CouponDetail.kt
@@ -1,10 +1,8 @@
 package com.test_application.hollyscompose.ui.coupon.coupon_detail
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.test_application.hollyscompose.data.Coupon
 
 @Composable
 fun CouponDetail(
