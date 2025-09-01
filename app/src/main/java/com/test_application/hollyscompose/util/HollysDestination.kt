9c6f4cb26952984371ceeb443c33e9f2129e89d5
JunghunKum <123260423+kumgold@users.noreply.github.com>
2025-09-01 22:08:49 +0900

    📝 delete a project

--- app/src/main/java/com/test_application/hollyscompose/util/HollysDestination.kt
+++
@@ -1,9 +0,0 @@
-package com.test_application.hollyscompose.util
-
-object HollysDestination {
-    const val HOME = "home"
-    const val SMART_ORDER = "smart_order"
-    const val HOLLYS_CARD = "hollys_card"
-    const val COUPON = "coupon"
-    const val COUPON_ID_KEY = "coupon_id"
-}
